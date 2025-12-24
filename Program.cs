74369fdea54cdde43d65d50a5412a8a366b66d73
unknown <bagorogilemphaka@gmail.com>
2025-12-24 07:36:36 +0200

    committing Program.cs and appsetting.json

---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using  SalonBookingApp1.Data;
+ 
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+try
+{
+    builder.Services.AddDbContext<SalonBookingAppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+}
+catch (Exception ex)
+{
+    Console.WriteLine("An error occurred while configuring the database context:" + ex.Message); throw;
+}
+
+
+
+
+builder.Services.AddDbContext<SalonBookingAppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
